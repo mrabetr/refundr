41e0210acecd8cc5c27993791fdc9fd4b1233259
Ben Ridley <benridley3@gmail.com>
2020-11-24 15:31:03 +0000

    home page controlla

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @trip = Trip.where(user: current_user, current: true)
   end
 end
