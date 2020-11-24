2824a4a6a172f8bda358abb73137c256e477afaf
rafik mrabet <mrabetr@hotmail.com>
2020-11-24 11:30:30 +0000

    updated trip model

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -4,5 +4,14 @@ class Trip < ApplicationRecord
   has_many :forms
 
   validates :arrival_date, :departure_date, :final_destination, presence: true
-  validate :departure_date_after_arrival_date
+  # validate :departure_date_after_arrival_date
+
+  # private
+
+  # def end_date_after_start_date
+  #   return xxx if departure_date.blank? || arrival_date.blank?
+  #   if departure_date < arrival_date
+  #     errors.add(:departure_date, "must be after the arrival date")
+  #   end
+  # end
 end
