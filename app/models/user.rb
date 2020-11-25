96245cf14426ee82de4991aecf4903f36f44bb0a
Maya Kowalewska <maja.kowalewska@yahoo.co.uk>
2020-11-25 16:32:21 +0000

    Geolocation activated

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,18 @@ class User < ApplicationRecord
   validates :title, :first_name, :last_name, :address, :passport_no, :passport_country, presence: true
   validates :passport_no, uniqueness: true
 
+  geocoded_by :current_sign_in_ip,
+  :latitude => :latitude, :longitude => :longitude
+  after_validation :geocode
+
+  def after_database_authentication
+    save
+  end
+
+  def user_location
+    Geocoder.search([latitude, longitude]).first.city
+  end
+  
   def passport_country_name
     country = ISO3166::Country[passport_country]
     country.translations[I18n.locale.to_s] || country.name
@@ -14,5 +26,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :trackable
 end
