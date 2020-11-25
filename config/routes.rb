d8e1d395d403c25ce55d33847dcd225f299cd658
Ben Ridley <benridley3@gmail.com>
2020-11-25 17:14:12 +0000

    nightmare of a button

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,12 @@ Rails.application.routes.draw do
 
   resources :trips do
     resources :receipts, only: [:new, :create]
-    resources :forms, only: [:new, :create]
+    resources :forms, only: [:create, :show]
   end
-  
+
   resources :receipts, only: [:show, :edit, :update, :destroy] do
   	resources :items, only: [:new, :create]
   end
-  
+
   resources :items, only: [:edit, :update, :destroy]
-  resources :forms, only: :show
 end
