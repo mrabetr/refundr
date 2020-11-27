123527326074097dc130518abc4adc3208fec827
rafik mrabet <mrabetr@hotmail.com>
2020-11-27 17:11:09 +0000

    fixes

--- app/services/ocr_service.rb
+++ app/services/ocr_service.rb
@@ -3,20 +3,20 @@ class OcrService
   # require 'google/cloud/vision'
   # Set up the envirenment with the API key
 
-  ENV['GOOGLE_APPLICATION_CREDENTIALS'] = Rails.root.join('app', 'services', 'refundr4.json').to_s
+  # ENV['GOOGLE_APPLICATION_CREDENTIALS'] = Rails.root.join('app', 'services', 'refundr4.json').to_s
 
   def detect_text(image_path)
     # Method requires the path of the image file to annotate
 
     # [START vision_text_detection]
     # Instantiates a client
-    image_annotator = Google::Cloud::Vision.image_annotator
+    # image_annotator = Google::Cloud::Vision.image_annotator
 
     # [START vision_text_detection_migration]
-    response = image_annotator.document_text_detection(
-      image:       image_path,
-      max_results: 1 # optional, defaults to 10
-    )
+    # response = image_annotator.document_text_detection(
+    #   image:       image_path,
+    #   max_results: 1 # optional, defaults to 10
+    # )
 
     # Performs text detection on the image file
     # response.responses.each do |res|
@@ -29,7 +29,7 @@ class OcrService
 
     # Performs text detection on image file returning the main string
     # puts response.responses.first.text_annotations.first.description
-    array = response.responses.first.text_annotations.first.description.split("\n")
+    # array = response.responses.first.text_annotations.first.description.split("\n")
     # p array
     # results = array.select { |text| text.include? "Тахable" }
     # p index = array.find_index { |item| item == "Total" }
