d504f8f9cca101fc2d229ec3eb2c52fb8686e0fb
rafik mrabet <mrabetr@hotmail.com>
2020-11-28 12:48:29 +0000

    ocr update

--- app/services/ocr_service.rb
+++ app/services/ocr_service.rb
@@ -1,17 +1,7 @@
+require 'google/cloud/vision'
 class OcrService
-  # Imports the Google Cloud client library
-  # require 'google/cloud/vision'
-  # Set up the envirenment with the API key
-
-  # ENV['GOOGLE_APPLICATION_CREDENTIALS'] = Rails.root.join('app', 'services', 'refundr4.json').to_s
-  # ENV['GOOGLE_APPLICATION_CREDENTIALS'] = JSON.parse(ENV.fetch('GOOGLE_APPLICATION_CREDENTIALS'))
-
   def detect_text(image_path)
-    # Method requires the path of the image file to annotate
-
-    # [START vision_text_detection]
     # Instantiates a client
-    # image_annotator = Google::Cloud::Vision::ImageAnnotator.new(credentials: JSON.parse(ENV.fetch('GOOGLE_APPLICATION_CREDENTIALS')))
     image_annotator = Google::Cloud::Vision.image_annotator
 
     # [START vision_text_detection_migration]
@@ -26,8 +16,6 @@ class OcrService
     #     puts text.description
     #   end
     # end
-    # [END vision_text_detection_migration]
-    # [END vision_text_detection]
 
     # Performs text detection on image file returning the main string
     # puts response.responses.first.text_annotations.first.description
@@ -51,5 +39,6 @@ class OcrService
   end
 end
 
-# image_path = 'services/photos/gucci3.jpg'
-# p OcrService.new.detect_text(image_path)
+image_path = 'services/photos/gucci3.jpg'
+service = OcrService.new
+service.detect_text(image_path)
